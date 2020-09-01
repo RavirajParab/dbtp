d8cc0feb07f693a2fa54395177862ae99710143b
Raviraj Parab <ravirajparab44@gmail.com>
2020-09-01 22:43:34 +0530

    done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   model: any = {};
 
-  onSubmit() {
+  login() {
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
   }
   constructor() { }
