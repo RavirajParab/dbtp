b2bd096a46e6b414fbd5dd64ba1b84437ba589ec
Raviraj Parab <ravirajparab44@gmail.com>
2020-09-01 12:17:14 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BuyComponent } from './buy/buy.component';
+import { PositionsComponent } from './positions/positions.component';
+import { StatementComponent } from './statement/statement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BuyComponent,
+    PositionsComponent,
+    StatementComponent
   ],
   imports: [
     BrowserModule,
