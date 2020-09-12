f2478476701decd683c2a6606aa97a6738c657e0
Raviraj Parab <ravirajparab44@gmail.com>
2020-09-12 18:19:49 +0530

    added positions logic

--- src/app/positions/positions.component.ts
+++ src/app/positions/positions.component.ts
@@ -1,4 +1,7 @@
+import { Position } from '../../IDBTP';
 import { Component, OnInit } from '@angular/core';
+import { DptpUtilityService } from '../dptp-utility.service';
+import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-positions',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PositionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private utility:DptpUtilityService) { }
+  Positions: Array<Position>=[];
+
+  // on position close click
+  
 
   ngOnInit(): void {
+    //make a call for the positions
+    const getSecurities = this.utility.getSecurities();
+    const getPositions = this.utility.getPositions();
+    //forkJoin both
+    forkJoin([getSecurities,getPositions])
+    .subscribe(result=>{
+      //@ts-ignore
+      const positionsWithPL = result[1].map(i=>{
+        return {
+          Symbol : i.Symbol,
+          Quantity: i.Quantity,
+          ID : i.id,
+          PL : (i.BP - result[0].find(x=>x.Symbol==i.Symbol).Price)*i.Quantity
+        }
+      });
+     // set the positions
+     this.Positions=positionsWithPL;
+     console.log(this.Positions);
+    },err=>{
+      console.log(err);
+    })
   }
-
 }
